e4223fef371fa167dbd0dc8c2c5d931ce246f445
jokyo02 <149650929+jokyo02@users.noreply.github.com>
2024-11-09 21:42:47 +0800

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -363,7 +363,7 @@ if (resUrl.pathname === "/c/api/start") {
 //};
 async function websocketPorxy(request) {
   const reqUrl = new URL(request.url);
-  reqUrl.hostname = "copilot.microsoft.com";
+  reqUrl.hostname = "mcop.pages.dev";
   reqUrl.protocol = "https:";
   reqUrl.port = "";
   const headers = new Headers(request.headers);
